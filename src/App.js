cc001c8adb391d6f02025bd92785686f8b377c49
Bardan <nepalibardan@gmail.com>
2023-11-30 00:17:24 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./router/Home";
 import Skills from "./router/Skills";
 import Projects from "./router/Projects";
